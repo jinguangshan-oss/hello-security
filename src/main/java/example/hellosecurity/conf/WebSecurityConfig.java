503b8c50780c0953a89550b88bc05abca33503b7
jinguangshan <qiaozhi31002@163.com>
2021-03-15 15:52:19 +0800

    20210315

--- src/main/java/example/hellosecurity/conf/WebSecurityConfig.java
+++ src/main/java/example/hellosecurity/conf/WebSecurityConfig.java
@@ -1,19 +1,29 @@
 package example.hellosecurity.conf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
-    ////配置正常认证
+
+    /**
+     * 配置用户
+     * @return
+     * @throws Exception
+     */
     @Bean
     public UserDetailsService userDetailsService() throws Exception {
-        // ensure the passwords are encoded properly
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(users.username("user").password("user").roles("USER").build());
@@ -21,39 +31,34 @@ public class WebSecurityConfig {
         return manager;
     }
 
-    //创建一个包含@Order的WebSecurityConfigurerAdapter实例，以指定应该首先考虑哪个WebSecurityConfigurerAdapter。
-    @Configuration
-    @Order(1)
-    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-        protected void configure(HttpSecurity http) throws Exception {
-            //http.antMatcher声明这个HttpSecurity将只适用于以/api/开头的url
-            http
-                .antMatcher("/admin/**")
-                .authorizeRequests(authorize -> authorize.anyRequest().hasRole("ADMIN"))
-                .httpBasic();
-//            http.authorizeRequests() // 开启 HttpSecurity 配置
-//                    .antMatchers("/admin/**").hasRole("ADMIN") // admin/** 模式URL必须具备ADMIN角色
-//                    .antMatchers("/user/**").access("hasAnyRole('ADMIN','USER')") // 该模式需要ADMIN或USER角色
-//                    .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") // 需ADMIN和DBA角色
-//                    .anyRequest().authenticated() // 用户访问其它URL都必须认证后访问（登录后访问）
-//                    .and().formLogin().loginProcessingUrl("/login").permitAll() // 开启表单登录并配置登录接口
-//                    .and().csrf().disable(); // 关闭csrf
-        }
-    }
-
-    //创建WebSecurityConfigurerAdapter的另一个实例。如果URL不是以/api/开头，将使用此配置。此配置在ApiWebSecurityConfigurationAdapter之后被考虑，因为有一个@Order值在1之后(没有@Order则默认为last，即最后一个)。
+    /**
+     * 配置 web安全配置适配器
+     */
     @Configuration
     public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable()
+
+                    //1、登录配置
                     .formLogin()
-                    .loginPage("/login.html")
+                    .loginPage("/login.html").loginProcessingUrl("/my/login")
                     .and()
+
+                    //2、登出配置
+                    .logout()
+                    .logoutUrl("/my/logout")
+//                    .addLogoutHandler(new CustomLogoutHandler())
+//                    .logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                    .and()
+
+                    //3、请求认证配置
                     .authorizeRequests()
                     .antMatchers("/login.html").permitAll()
                     .anyRequest().authenticated();
         }
     }
+
+
 }
--- src/main/java/example/hellosecurity/controller/HostController.java
+++ src/main/java/example/hellosecurity/controller/HostController.java
@@ -1,20 +1,48 @@
 package example.hellosecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Slf4j
 public class HostController {
 
-    @RequestMapping(path = "/admin/host", method = RequestMethod.GET)
-    String hello() {
-        return "hello";
+    String UNKNOWN = "unknown";
+
+    @RequestMapping(path = "/host", method = RequestMethod.GET)
+    String host() {
+        //从上下文中取出HttpServletRequest
+        HttpServletRequest  request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        //定义url可能对应的Header中的key数组，下标越小优先级越大
+        String[] strs = {
+                "X-Requested-For",
+                "X-Forwarded-For",
+                "Proxy-Client-IP",
+                "WL-Proxy-Client-IP",
+                "HTTP_CLIENT_IP",
+                "HTTP_X_FORWARDED_FOR"};
+        //遍历key数组
+        String clientIp = null;
+        for(String str : strs){
+            if(StringUtils.hasText(request.getHeader(str)) && !request.getHeader(str).equals("unknown")){
+                clientIp = request.getHeader(str);
+                break;
+            }
+        }
+        //返回
+        return clientIp == null ? request.getRemoteAddr() : clientIp;
+
     }
 
-    @RequestMapping(path = "/user/host", method = RequestMethod.GET)
-    String other() {
-        return "hello other";
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    String main() {
+        return "main page";
     }
-
 }
